7266cc5eaa816d41a165dae32988e0d85326fadc
TinyDolphin-Con <tiny_dolphin@163.com>
2023-03-31 15:54:56 +0800

    feat(lvim): add some function

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,6 +15,8 @@ keymap('n', 'W', ':w<CR>', { noremap = true, silent = true })
 keymap('n', 'Q', ':q<CR>', { noremap = true, silent = true })
 -- 快速保存并退出
 keymap('n', 'qw', ':wq<CR>', { noremap = true, silent = true })
+-- 快速保存并退出所有窗口
+keymap('n', 'qa', ':wqa<CR>', { noremap = true, silent = true })
 -- 选中全文（普通模式、可视模式和选择模式）
 keymap({'n', 'x'}, 'VV', '<Esc>gg0vG$<CR>', { noremap = true, silent = true })
 -- 取消搜索高亮
